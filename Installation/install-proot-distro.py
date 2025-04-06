e9045a05789f68df6308e6a909b438ef3696c161
felix-the-cat177 <mm5809562@gmail.com>
2025-04-06 15:49:35 -0300

    Add files via upload

---
+++ Installation/install-proot-distro.py
@@ -0,0 +1,94 @@
+import os
+import time
+import json
+import sys
+import subprocess
+from colorama import init, Fore
+from datetime import datetime
+
+# horas & sistema & shell
+agora = datetime.now()
+system = sys.platform
+update = "pkg update && apt update"
+clear = "clear"
+upgrade = "pkg upgrade -y ; apt upgrade -y"
+proot_distro = "pkg install proot-distro -y ; apt install proot-distro -y"
+
+init()
+
+# banner e cor do Gato Félix
+banner = subprocess.check_output(['figlet', "Felix The Cat"]).decode('utf-8')
+vermelho = "\033[91m"
+amarelo = Fore.YELLOW
+
+def limpar_tela():
+  if system == "windows" or system == "Windows":
+    cls = "cls"
+    os.system(cls)
+  else:
+    clear = "clear"
+    os.system(clear)
+
+def system_windows():
+  if system == "windows" or system == "Windows":
+    print("[!] Esse Script Não foi feito para windows apenas para Termux!")
+    exit()
+
+def system_termux():
+  if system == "linux" and os.path.exists("/data/data/com.termux"):
+    return "Termux"
+  else:
+    return system
+    
+def debian_system():
+  print(f"{amarelo}[/] Executando pkg update & apt update...{Fore.RESET}")
+  time.sleep(1)
+  os.system(update)
+  print(f"{amarelo}[✓] Atualização concluida!{Fore.RESET}")
+  time.sleep(1.5)
+  os.system(clear)
+  print(f"{amarelo}[/] Executando pkg upgrade & apt upgrade...{Fore.RESET}")
+  os.system(upgrade)
+  print(f"{amarelo}[✓] Instalação concluida!{Fore.RESET}")
+  time.sleep(1.5)
+  print(f"{amarelo}[/] Executando pkg install proot-distro...{Fore.RESET}")
+  os.system(proot_distro)
+  print(f"{amarelo}[✓] Instalação concluida!{Fore.RESET}")
+  time.sleep(1.5)
+  os.system(clear)
+  print(f"{amarelo}[/] Instalando o Debian...{Fore.RESET}")
+  os.system("proot-distro install debian")
+  time.sleep(1.5)
+  os.system(clear)
+  print(f"{amarelo}[/] Login no Debian...{Fore.RESET}")
+  time.sleep(1.5)
+  os.system("proot-distro login debian -- apt update && apt install git python && git clone https://github.com/felix-the-cat177/Linux-installation-2025.git && cd Linux-installation-2025 && cd Installation && python setup-install-debian-cinammon.py")
+  with open("user.json", "r") as arquivos_json:
+    usuario = json.load(arquivos.json)
+    print(f"{amarelo}[!] Qual cinnamon você deseja?{Fore.RESET}")
+    print("1- Cinammon básico")
+    print("2- Cinammon full")
+    cinnamon_opcao = input("Digite a opcão: ")
+    if cinnamon_opcao == "1":
+      os.system(f"proot-distro login --user {user} debian -- sudo apt update && sudo apt install cinnamon -y")
+    elif cinnamon_opcao == "2":
+      os.system(f"proot-distro login --user {user} debian -- sudo apt install cinnamon-desktop-environment -y")
+  
+termux = system_termux()
+
+limpar_tela()
+print(f"{banner}")
+print(f"{amarelo}[✓] Script By Diogo{Fore.RESET}")
+print(f"{amarelo}[✓] Hora atual: {agora.hour:02d}:{agora.minute:02d}:{agora.second:02d}{Fore.RESET}")
+system_windows()
+try:
+  if termux == "Termux":
+    print("")
+    print("1- Debian + Cinammon")
+    opcao = int(input("Digite a opção: "))
+    if opcao == 1:
+      debian_system()
+except ValueError:
+  print(f"{vermelho}[X] opção inválida coloque apenas números...")
+  time.sleep(1)
+  
---
+++ Installation/setup-install-debian-cinammon.py
@@ -0,0 +1,92 @@
+import os
+import time
+import json
+import sys
+import subprocess
+from colorama import init, Fore
+from datetime import datetime
+
+# horas & sistema & shell
+agora = datetime.now()
+system = sys.platform
+nano = "apt install nano -y"
+adduser = "apt install adduser -y"
+sudo = "apt install sudo -y"
+clear = "clear"
+upgrade = "apt upgrade -y"
+
+int()
+
+# banner e cor do Gato Félix
+banner = subprocess.check_output(['figlet', "Felix The Cat"]).decode('utf-8')
+vermelho = "\033[91m"
+amarelo = Fore.YELLOW
+
+def limpar_tela():
+  if system == "windows" or system == "Windows":
+    os.system("cls")
+  else:
+    os.system(clear)
+
+def system_windows():
+  if system == "windows" or system == "Windows":
+    print("[!] o script não foi feito para Windows apenas para termux!")
+
+def system_termux():
+  if system == "linux" and os.path.exists("/data/data/com.termux"):
+    return "Termux"
+  else:
+    return system
+    
+termux = system_termux()
+
+limpar_tela()
+print(banner)
+print(f"{amarelo}[✓] Script By Diogo{Fore.RESET}")
+print(f"{amarelo}[✓] Hora atual: {agora.hour:02d}:{agora.minute:02d}:{agora.second:02d}{Fore.RESET}")
+system_windows()
+time.sleep(1.5)
+try:
+  if termux == "Termux":
+    print(f"{amarelo}[/] Instalando nano{Fore.RESET}")
+    time.sleep(0.5)
+    os.system(nano)
+    print(f"{amarelo}[✓] Instalação nano concluida!{Fore.RESET}")
+    time.sleep(0.5)
+    print(f"{amarelo}[/] Instalando adduser{Fore.RESET}")
+    time.sleep(0.5)
+    os.system(adduser)
+    print(f"{amarelo}[✓] Instalação adduser concluida!{Fore.RESET}")
+    time.sleep(0.5)
+    print(f"{amarelo}[/] Instalando sudo{Fore.RESET}")
+    time.sleep(0.5)
+    os.system(sudo)
+    print(f"{amarelo}[✓] Instalação sudo concluida!{Fore.RESET}")
+    time.sleep(0.5)
+    print(f"{amarelo}[/] Atualizando apt upgrade{Fore.RESET}")
+    os.system(upgrade)
+    time.sleep(0.5)
+    print(f"{amarelo}[✓] Todos os pacotes e upgrade instalado!{Fore.RESET}")
+    time.sleep(2.5)
+    os.system(clear)
+    user = input("Digite o seu usuário: ")
+    full_name = input("Digite o full-name do usuário: ")
+    senha = input("Digite uma senha para o usuário: ")
+    usuario = {
+      "user": user,
+      "full_name": full_name,
+      "senha": senha
+    }
+    with open("user.json", "w") as arquivos:
+      json.dump(usuario, arquivos, indent=4)
+    os.system(f"useradd -M -s /bin/bash -c '{full_name}' {user}")
+    os.system(f"echo {user}:{senha} | chpasswd")
+    os.system(f"echo '{user} ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo")
+    os.system(f"echo 'proot-distro login --user {user} debian' >> $PREFIX/data/data/com.termux/files/usr/bin/debian")
+    os.system("chmod +x /data/data/com.termux/files/usr/bin/debian")
+    os.system("exit")
+except ValueError:
+  print(f"{vermelho}[X] ERROR ValueError{Fore.RESET}")
+except KeyboardInterrupt:
+  print(f"{vermelho}[!] Forçamento com CTRL + C{Fore.RESET}")
+  exit()
