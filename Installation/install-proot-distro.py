89739802f6746eff623fa5e016f45227f3b6985c
felix-the-cat177 <mm5809562@gmail.com>
2025-04-06 16:35:08 -0300

    update Installation/install-proot-distro.py

--- Installation/install-proot-distro.py
+++ Installation/install-proot-distro.py
@@ -63,7 +63,8 @@ def debian_system():
   time.sleep(1.5)
   os.system("proot-distro login debian -- bash -c 'cd / && apt update && apt install git python3 && git clone https://github.com/felix-the-cat177/Linux-installation-2025.git && cd Linux-installation-2025 && cd Installation && python setup-install-debian-cinammon.py'")
   with open("Dados/user.json", "r") as user_json:
-    user = json.load(user_json)
+    usuario = json.load(user_json)
+    user =usuario["user"]
     os.system(clear)
     print(f"{amarelo}[!] Qual cinnamon você deseja?{Fore.RESET}")
     print("1- Cinammon básico")
