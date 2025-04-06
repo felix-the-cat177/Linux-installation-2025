5486233548fb5e0969c82de8355428dda850c7dc
felix-the-cat177 <mm5809562@gmail.com>
2025-04-06 16:22:29 -0300

    update Installation/setup-install-debian-cinammon.py

--- Installation/setup-install-debian-cinammon.py
+++ Installation/setup-install-debian-cinammon.py
@@ -79,8 +79,8 @@ try:
     }
     user_atual = os.path.dirname(__file__)
     user_arquivo = os.path.join(user_atual, "Dados", "user.json")
-    with open(user_arquivo, "r") as arquivo_json:
-    usuario = json.load(arquivo_json)
+    with open("Dados/user.json", "w") as arquivo:
+      json.dump(usuario, arquivo, indent=4)
     os.system(f"useradd -M -s /bin/bash -c '{full_name}' {user}")
     os.system(f"echo {user}:{senha} | chpasswd")
     os.system(f"echo '{user} ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo")
