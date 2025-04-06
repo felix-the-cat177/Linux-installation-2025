851ea6d033f303c751f83e26d630d42d14f7a968
felix-the-cat177 <mm5809562@gmail.com>
2025-04-06 16:42:37 -0300

    update Installation/install-proot-distro.py

--- Installation/install-proot-distro.py
+++ Installation/install-proot-distro.py
@@ -71,7 +71,7 @@ def debian_system():
     print("2- Cinammon full")
     cinnamon_opcao = input("Digite a opcão: ")
     if cinnamon_opcao == "1":
-      os.system(f"proot-distro login --user root debian -- apt update && apt install cinnamon -y")
+      os.system(f"proot-distro login --user root debian -- apt update && apt install cinnamon-desktop-environment-minimal -y")
     elif cinnamon_opcao == "2":
       os.system(f"proot-distro login --user root debian -- apt install cinnamon-desktop-environment -y")
   
